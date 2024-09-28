40ddeb0ea8f1d51371912cf66198bd14ff93fd78
Abdalluh28 <Abdok7496@gmail.com>
2024-09-28 16:52:21 +0300

    Update origins again

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,7 +1,5 @@
 const allowedOrigins = [
-    'http://localhost:3000',
-    'http://localhost:3001',
-    'https://electronic-store-g5lb.vercel.app/',
+    'https://electronic-store-g5lb.vercel.app', // Your frontend domain
+    'https://electronic-store-eight.vercel.app', // Your backend domain
 ];
-
 module.exports = allowedOrigins;

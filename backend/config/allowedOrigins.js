cbd2fab2890aecf0e07d1f86602ae5d1e93251d8
Abdalluh28 <Abdok7496@gmail.com>
2024-09-28 11:46:30 +0300

    Update CORS configuration

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,6 +1,8 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
+    'https://electronic-store-8qmkdkgqc-abdalluhs-projects.vercel.app/',
+    'https://electronic-store-phi.vercel.app/'
 ];
 
 module.exports = allowedOrigins;

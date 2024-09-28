6500a73fb72fa9a40d4d6b6d0f9daad9f9efd834
Abdalluh28 <Abdok7496@gmail.com>
2024-09-28 12:43:01 +0300

    Update cors

--- backend/config/allowedOrigins.js
+++ backend/config/allowedOrigins.js
@@ -1,8 +1,7 @@
 const allowedOrigins = [
     'http://localhost:3000',
     'http://localhost:3001',
-    'https://electronic-store-8qmkdkgqc-abdalluhs-projects.vercel.app/',
-    'https://electronic-store-phi.vercel.app/'
+    'https://electronic-store-g5lb.vercel.app/',
 ];
 
 module.exports = allowedOrigins;

6afab635df5b16a9633c07306371da1342f5351e
Abdalluh28 <Abdok7496@gmail.com>
2024-09-28 11:58:07 +0300

    Update client url

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -58,7 +58,7 @@ const sendVerificationEmailRegister = async (newUser, accessToken) => {
         from: 'abdok7496@gmail.com',
         to: newUser.email,
         subject: 'Verify your email',
-        text: `Please verify your email by clicking the following link: http://localhost:3001/auth/verify/${newUser._id}/${accessToken}`
+        text: `Please verify your email by clicking the following link: ${process.env.CLIENT_URL}/auth/verify/${newUser._id}/${accessToken}`
     };
 
     return new Promise((resolve, reject) => {
@@ -100,7 +100,7 @@ const sendVerificationEmailLogin = asyncHandler(async (req, res) => {
         from: 'abdok7496@gmail.com',
         to: existingUser.email,
         subject: 'Verify your email',
-        text: `Please verify your email by clicking the following link: http://localhost:3001/auth/verify/${existingUser._id}/${accessToken}`
+        text: `Please verify your email by clicking the following link: ${process.env.CLIENT_URL}/auth/verify/${existingUser._id}/${accessToken}`
     };
 
     try {

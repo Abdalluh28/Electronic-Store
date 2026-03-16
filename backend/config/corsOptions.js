882ffb41717bebb25fa46924831b703d339c35f1
Abdalluh28 <99888044+Abdalluh28@users.noreply.github.com>
2026-03-16 17:03:01 +0200

    Refactor CORS options for better origin handling

--- backend/config/corsOptions.js
+++ backend/config/corsOptions.js
@@ -1,16 +1,16 @@
-const allowedOrigins = require("./allowedOrigins");
+const allowedOrigins = require('./allowedOrigins');
 
 const corsOptions = {
-    origin: (origin, callback) => {
-        if(allowedOrigins.indexOf(origin) !== -1 || !origin) {
-            callback(null, true);
-        } else {
-            callback(new Error("Not allowed by CORS"));
-        }
-    },
-    credentials: true,
-    optionsSuccessStatus: 200
-}
+  origin: (origin, callback) => {
+    // allow requests with no origin (like Postman, mobile apps, server-to-server)
+    if (!origin || allowedOrigins.some(o => o instanceof RegExp ? o.test(origin) : o === origin)) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true,
+  optionsSuccessStatus: 200
+};
 
-
-module.exports = corsOptions
+module.exports = corsOptions;
